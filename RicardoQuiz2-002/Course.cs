0f2dcb09f9fd828041d29014c58f6d8555839814
ricardojdsilva <ricardojd@gmail.com>
2021-03-19 12:01:59 -0700

    test

---
+++ RicardoQuiz2-002/Course.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RicardoQuiz2_002
+{
+    class Course
+    {
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }
+        public double[] mark = new double[10];
+        public Course(string id, string name, double[] grade)
+        {
+            CourseID = id;
+            CourseName = name;
+
+            for (int i = 0; i < grade.Length; i++)
+            {
+                mark[i] = grade[i];
+
+            }
+
+        }
+
+        public double GetAverage
+        {
+            get
+            {
+                double sum = 0.0;
+
+
+                for (int i = 0; i < mark.Length; i++)
+                {
+                    sum += mark[i];
+                }
+
+                return sum / mark.Length;
+
+            }
+
+        }
+
+        public double GetLowest
+        {
+            get
+            {
+
+                double minValue = mark[0];
+                for (int i = 1; i < mark.Length; i++)
+                {
+                    if (mark[i] < minValue)
+                    {
+                        minValue = mark[i];
+                    }
+                }
+                 return minValue;
+            }
+
+        }
+
+
+
+
+        public override string ToString()
+        {
+           
+            return "Course Id:" + CourseID + "Course name: " + CourseName +
+                   "\nAverage marks obtained : " + GetAverage.ToString("##.##") +
+                   "\nLowest marks : " + GetLowest.ToString("##.##");
+        }
+
+    }
+}
---
+++ RicardoQuiz2-002/CourseApp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RicardoQuiz2_002
+{
+    class CourseApp
+    {
+        static void Main(string[] args)
+        {
+
+            const int qty_students = 10;
+            int j = 0;
+            double dmarks = 0.0;
+            string CourseName = "";
+            string CourseID = "";
+            double[] vGrade = new double[10];
+
+
+            Course[] NCourse = new Course[qty_students];
+
+            Console.Write("Enter course Id :");
+            CourseID = Console.ReadLine();
+            //ClassCourse.CourseID = Convert.ToInt32(Console.ReadLine());
+
+            // NewStudents[i] = new Student() { StudentID = i };
+
+            Console.Write("Enter course name :");
+          
+           CourseName = Console.ReadLine();
+
+
+            while (j < qty_students)
+            {
+
+                // Enter marks for student# 3
+                Console.Write("Enter mark for student {0} :", j+1);
+
+                vGrade[j] = Convert.ToDouble(Console.ReadLine());
+
+                j++;
+            }
+
+
+            NCourse[0] = new Course(CourseID, CourseName, vGrade);
+
+            Console.WriteLine(NCourse[0].ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
